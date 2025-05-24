59459fb50b0a8c00200db262ffbe6fac281d5153
DeanWay <deanway93@gmail.com>
2025-05-24 10:20:30 -0400

    Bump version from 0.5.0 to 0.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="coverage_threshold",
-    version="0.5.0",
+    version="0.6.0",
     author="Dean Way",
     description="Tools for coverage threshold limits",
     long_description=long_description,
