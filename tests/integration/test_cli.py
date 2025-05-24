ef71fa2f9dfc49ee72409b0831c286db1c39044a
DeanWay <deanway93@gmail.com>
2025-05-24 09:41:44 -0400

    add integration test for old pyproject.toml key

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -29,6 +29,21 @@ def test_cli_runs_successfully_on_example_project() -> None:
     assert process.stdout == SUCCESS_MESSAGE.encode("utf-8")
 
 
+# backwards compatibilty for before this project became compliant with pep518
+def test_cli_runs_successfully_on_example_project_with_legacy_pyproject_format() -> (
+    None
+):
+    process = subprocess.run(
+        ["coverage-threshold", "--config", "legacy.pyproject.toml"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=EXAMPLE_PROJECT_PATH,
+    )
+    assert process.returncode == 0
+    assert process.stderr == b""
+    assert process.stdout == SUCCESS_MESSAGE.encode("utf-8")
+
+
 def test_cli_fails() -> None:
     process = subprocess.run(
         ["coverage-threshold", "--line-coverage-min", "100.0"],
