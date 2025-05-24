3b62bcef465b04856308a15a78dbd3e196b56578
DeanWay <deanway93@gmail.com>
2025-05-24 10:11:58 -0400

    add missing return types

--- tests/unit/config/test_read_config.py
+++ tests/unit/config/test_read_config.py
@@ -4,20 +4,20 @@ from decimal import Decimal
 from coverage_threshold.cli.read_config import read_config
 
 
-def test_read_config_parses_default_pyproject_format():
+def test_read_config_parses_default_pyproject_format() -> None:
     config = read_config(path_to_test_config("pyproject.toml"))
     assert config.line_coverage_min == Decimal("75.0")
     assert config.modules is None
 
 
 # backwards compatibilty for before this project became compliant with pep518
-def test_read_config_parses_legacy_pyproject_format():
+def test_read_config_parses_legacy_pyproject_format() -> None:
     config = read_config(path_to_test_config("legacy.pyproject.toml"))
     assert config.line_coverage_min == Decimal("75.0")
     assert config.modules is None
 
 
-def test_read_config_parses_complex_pyproject_format():
+def test_read_config_parses_complex_pyproject_format() -> None:
     config = read_config(path_to_test_config("complex.pyproject.toml"))
     assert config.line_coverage_min == Decimal("95.0")
     assert config.file_line_coverage_min == Decimal("95.0")
